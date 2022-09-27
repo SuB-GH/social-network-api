32cb6ee6c7995dea221ef81c3fb3d06587bedb5f
Susan Brown <brown684408@gmail.com>
2022-09-27 18:29:51 -0500

    update thought and user routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,18 +1,29 @@
 const router = require('express').Router();
 const {
     // add routes here
-  } = require('../../controllers/thought-controller');
-
-  
+} = require('../../controllers/thought-controller');
 
 // /api/thoughts/<userId>
 router.route('/:userId').post(addThought);
 
 // /api/thoughts/<userId>/<thoughtId>
 router
-.route('/:userId/:thoughtId')
-.put(addReply)
-.delete(removeThought);
+    .get(getAllThoughts) //GET to get all thoughts
+    .route('/:userId/:thoughtId')
+    .delete(removeThought) //DELETE to remove a thought by its _id
+    .get(getThoughtById) // GET to get a single thought by its _id
+    .post(createThought) //POST to create a new thought, example below:
+    // {
+    //     "thoughtText": "Here's a cool thought...",
+    //     "username": "lernantino",
+    //     "userId": "5edff358a0fcb779aa7b118b"
+    //   }
+    .put(updateThought); //PUT to update a thought by its _id
+
+    // router.route('/:api/thoughts/:thoughtId/reactions);
+    //     .post(createReaction) //POST to create a reaction stored in a single thought's reactions array field
+    //     .delete(removeReaction)) //DELETE to pull and remove a reaction by the reaction's reactionId value
+
 
 router.route('/:userId/:thoughtId/:replyId').delete(removeReply);
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,20 +2,30 @@ const router = require('express').Router();
 // instead of importing the entire object, we've destructured out just the object method names
 const {
     // add methods here
-  } = require('../../controllers/user-controller');
+} = require('../../controllers/user-controller');
 
 // Set up GET all and POST at /api/users
 // this implements the object methods above into the routes. Also, the name of the controller method is the callback (in the parentheses) as a result of using req/res as parameters
 router
-  .route('/')
-  .get(getAllUser)
-  .post(createUser);
+    .route('/')
+    .get(getAllUser) //GET all users
+    .post(createUser); //POST a new user, example below
+//{
+//   "username": "lernantino",
+//   "email": "lernantino@gmail.com"
+// }
+
 
 // Set up GET one, PUT, and DELETE at /api/users/:id
 router
-  .route('/:id')
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
+    .route('/:userId')
+    .get(getUserById) //GET a single user by its _id and populated thought and friend data
+    .put(updateUser) //PUT to update a user by its _id
+    .delete(deleteUser); //DELETE to remove user by its _id
+
+router.route('/:userId/friends/:friendId')
+    .post(addFriend) //POST to add a new friend to a user's friend list
+    .delete(deleteFriend); //DELETE to remove a friend from a user's friend list
+
 
 module.exports = router;
