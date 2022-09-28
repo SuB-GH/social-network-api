65f37339e34180c158225c0b3704a2aba3db3090
Susan Brown <brown684408@gmail.com>
2022-09-28 11:23:38 -0500

    work on thought-controller methods

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,6 +2,23 @@ const { Thought, User } = require('../models');
 
 // this is a thoughtController object, containing two methods for adding and removing thoughts
 const thoughtController = {
+
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .populate({ // this shows the actual thought, not just the id
+                path: 'thoughts', // is it singular or plural?
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 }) // this will sort by id value
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+   
     // add thought to user. the $push method (adds data to an array) and allows us to add the thought's _id to the specific user we want to update. (MongoDB-based functions start with $ to make them stand out)
     addThought({ params, body }, res) {
         console.log(body);
@@ -25,31 +42,31 @@ const thoughtController = {
 
     addThought({ params, body }, res) {
         Thought.findOneAndUpdate(
-          { _id: params.thoughtId },
-          { $push: { replies: body } },
-          { new: true, runValidators: true }
+            { _id: params.thoughtId },
+            { $push: { replies: body } },
+            { new: true, runValidators: true }
         )
-          .then(dbUserData => {
-            if (!dbUserData) {
-              res.status(404).json({ message: 'No user found with this id!' });
-              return;
-            }
-            res.json(dbUserData);
-          })
-          .catch(err => res.json(err));
-      },
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
 
-      // remove thought
-removeThought({ params }, res) {
-    Thought.findOneAndUpdate(
-      { _id: params.thoughtId },
-// $pull operator removes the specific thought from the replies array where the thoughtId matches the value of params.thoughtId passed in from the route
-      { $pull: { replies: { thoughtId: params.thoughtId } } },
-      { new: true }
-    )
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => res.json(err));
-  },
+    // remove thought
+    removeThought({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            // $pull operator removes the specific thought from the replies array where the thoughtId matches the value of params.thoughtId passed in from the route
+            { $pull: { replies: { thoughtId: params.thoughtId } } },
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
 
     // remove thought. first we delete the thought (while also returning it's data), then we'll use its _id to remove it from the user using $pull 
     removeThought({ params }, res) {
